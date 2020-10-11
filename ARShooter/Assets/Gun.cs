e7b98c85678bc5c0e0daf7ac24781b54b105a913
Burakhan Alagöz <burakhanalagoz@gmail.com>
2020-10-11 17:45:33 +0300

    Initial commit

---
+++ ARShooter/Assets/Gun.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+    public GameObject bulletPrefab;
+	private void Update () {
+        if (Input.GetMouseButton(0)){
+            GameObject go = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+            go.GetComponent<Rigidbody>().AddForce(transform.forward * 15, ForceMode.Impulse);
+        }
+	}
+}
